8586111322cad16129167442c360b64a3b31b5a8
ldawa759 <ldawa759@gmail.com>
2025-07-09 12:06:41 -0400

    hihi

---
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+    string variable1 = "Hello ";
+    int var = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        string variable2 = "World";
+        Debug.Log(variable1 + variable2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(var);
+        var++;
+    }
+}
