1180cd98f9a239e7a073cef7b3f0669fa318af67
ldawa759 <ldawa759@gmail.com>
2025-07-10 12:17:34 -0400

    ddd

--- Assets/Geo_Quest/Scenes/GeoController.cs
+++ Assets/Geo_Quest/Scenes/GeoController.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class GeoController : MonoBehaviour
 {
     string variable1 = "Hello ";
     int var = 3;
+    private Rigidbody2D rb;
+    public int speed = 5;
+
     // Start is called before the first frame update
-    void Start()
+     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         string variable2 = "World";
         Debug.Log(variable1 + variable2);
     }
@@ -16,8 +21,28 @@ public class GeoController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(var);
-        var++;
-        transform.position += new Vector3(0.005f, 0, 0);
+        float xSpeed = Input.GetAxis("Horizontal");
+        //Debug.Log(xSpeed);
+        rb.velocity = new Vector2(xSpeed * speed, rb.velocity.y);
+
+        /*
+       if (Input.GetKeyDown(KeyCode.W))
+        {transform.position += new Vector3(0, 1, 0); }
+       if (Input.GetKeyDown(KeyCode.S))
+        { transform.position += new Vector3(0, -1, 0); }
+       if (Input.GetKeyDown(KeyCode.A))
+        { transform.position += new Vector3(-1, 0, 0); }
+        if (Input.GetKeyDown(KeyCode.D))
+        { transform.position += new Vector3(1, 0, 0); }*/
     }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Hit");
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+
+    }
+
+
 }
