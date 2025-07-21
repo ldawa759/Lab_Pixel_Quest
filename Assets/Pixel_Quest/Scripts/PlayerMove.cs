a253a26a50b30a3c0c2aabc17b25e9b6b542fbd7
Yuusiiii <smuggy3v@gmail.com>
2025-07-21 10:25:15 -0400

    savwe

--- Assets/Pixel_Quest/Scripts/PlayerMove.cs
+++ Assets/Pixel_Quest/Scripts/PlayerMove.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    public int speed = 5;
+    private SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xSpeed = Input.GetAxis("Horizontal");
+        //Debug.Log(xSpeed);
+        rb.velocity = new Vector2(xSpeed * speed, rb.velocity.y);
+
+        if (xSpeed > 0)
+        {
+            sr.flipX = true;
+        }
+        else if (xSpeed < 0)
+        {
+            sr.flipX = false;
+        }
     }
 }
--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string nextLevel = "Geo_Quest_Scene_1 1";
+    
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        switch (collision.tag)
+        {
+            case "Death":
+                {
+                    string thisLevel = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(thisLevel);
+                    Debug.Log("Player Has Died");
+                    break;
+                }
+            case "Finish":
+                {
+                    SceneManager.LoadScene(nextLevel);
+                    break;
+                }
+        }
     }
 }
