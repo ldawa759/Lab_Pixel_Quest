a66286ae930c61e79bab13ed1f673e68c8b78504
ldawa759 <ldawa759@gmail.com>
2025-07-22 12:11:20 -0400

    save

--- Assets/Pixel_Quest/Scripts/PlayerStats.cs
+++ Assets/Pixel_Quest/Scripts/PlayerStats.cs
@@ -5,12 +5,32 @@ using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
+    private int _health = 3;
+    private int coinCounter = 0;
     public string nextLevel = "Geo_Quest_Scene_1 1";
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         switch (collision.tag)
         {
+            case "Health":
+                {
+                    _health++;
+                    Destroy(collision.gameObject);
+                    break;
+                }
+
+
+
+
+            case "Coin": 
+                {
+                    coinCounter++;
+                    Destroy(collision.gameObject);
+                    break;
+
+                }
+
             case "Death":
                 {
                     string thisLevel = SceneManager.GetActiveScene().name;
